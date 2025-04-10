4b14b1136db51f95de165dd02ed9dd1c7c6bd508
JosefineChristine <joro0003@stud.kea.dk>
2025-04-10 14:07:36 +0200

    fjernet noget i css og html

--- src/main/java/org/example/aiproject/config/ApiDataPrinter.java
+++ src/main/java/org/example/aiproject/config/ApiDataPrinter.java
@@ -13,14 +13,14 @@
 //        this.foodWasteService = foodWasteService;
 //    }
 //
-////    @Override
-////    public void run(String... args) throws Exception {
-//////        foodWasteService.fetchFoodWasteByStoreId("abc74dcc-1696-4e94-bcef-d00c44fb58a9")
-//////                .block()
-//////                .forEach(System.out::println);
-//////        System.out.println(foodWasteService.fetchStoreById("abc74dcc-1696-4e94-bcef-d00c44fb58a9").block());
-//////        foodWasteService.fetchStores().forEach(System.out::println);
-////
-////    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        foodWasteService.fetchFoodWasteByStoreId("abc74dcc-1696-4e94-bcef-d00c44fb58a9")
+//                .block()
+//                .forEach(System.out::println);
+//        System.out.println(foodWasteService.fetchStoreById("abc74dcc-1696-4e94-bcef-d00c44fb58a9").block());
+//        foodWasteService.fetchStores().forEach(System.out::println);
+//
+//    }
 //
 //}
